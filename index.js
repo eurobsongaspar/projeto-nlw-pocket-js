b94fed40d5e96e8fa667aa5dbc812b562834a2ad
Robson Gaspar <orobsongaspar@gmail.com>
2024-09-10 05:32:37 -0300

    Fundamentos 003

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-// Array, Objetos
 let meta = {
     value: "Ler um livro por mês",
-    checked: false,
-    log: (info) =>{
-        console.log(info)
-    }
-
+    checked: true,
 }
 
-meta.log(meta.value)
+let metas = [
+    meta,
+    {
+        value:"Caminhar 20 minutos todos os dias",
+        checked:false,
+    }
+]
 
-// function // arrow function
-const criarMeta = () => {}
-
-function criarMeta(){}
+console.log(metas[1].value)
