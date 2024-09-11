d43cb0dd697325ec81861587143b8f26fb092460
Robson Gaspar <orobsongaspar@gmail.com>
2024-09-11 10:18:42 -0300

    Fundamentos 005

--- aula.md
+++ aula.md
@@ -62,3 +62,4 @@ Operadores de concatenação
 
 # Condicionais
 
+- [*] Switch
--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const start = () => {
-    let count = 0 // inicia o contador com o valor 0
-    while (count < 10){ // WHILE é estrutura de repetição. Verifica se a var count é menor ou igual a 10
-    console.log(count) // Como foi atribuido 0 a count inicialmente, o console atribui o valor 0
-    count = count + 1 // Nessa linha a var count recebe seu valor +1, OU SEJA 0+1
-    } // Quando chega aqui, o app volta no While, que é uma repetição e a count já não é 0 e sim 1, e isso se repetirá por 10 vezes e a cada vez, o valor de count será somado a 1.
+    
+    while (true){ // cria a repetição infinita, por se tratar do valor true
+        let opcao = "sair" // Cria a var OPCAO com a string CADASTRAR (TROQUEI PRA SAIR para parar a repeticao)
+        switch(opcao){ // a CONDICIONAL switch recebe o valor da var OPCAO, que é CADASTRAR
+            case "cadastrar": // Cria o caso CADASTRAR
+                console.log("Vamos cadastrar")// O console vai mostrar a mensagem VAMOS CADASTRAR
+                break // Para o caso CADASTRAR
+
+            case "listar":
+                console.log("Vamos listar")
+                break
+
+            case "sair":
+                console.log("Sair")
+                return
+                
+        }
+
+    }
 }
 start()
 
